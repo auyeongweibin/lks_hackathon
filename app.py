34d81f3b60c888afebebace49f47a7bac6737426
Wei Bin <auyeongweibin@gmail.com>
2023-08-26 15:07:29 +0800

    added TODOs

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS
 import requests
-from config import SEARCH_URL
+from config import SEARCH_URL, GET_RESOURCE
 
 def create_app(config_filename):
     application = app = Flask(__name__, static_url_path='')
@@ -23,21 +23,24 @@ def search():
     key = request.args.get('key')
     search_key = key.replace(' ', '+')
     url = SEARCH_URL.replace('[insert_search_here]', search_key)
+    resource_url = GET_RESOURCE
     res = requests.get(url).json()
-    doc = res['docs']
-    if len(doc) == 0:
+    docs = res['docs']
+    if len(docs) == 0:
         return {'Message': 'Sorry we could not find any related resources'}
     result = [
         {
-            'type': d['pnx']['display']['type'],
-            'title': d['pnx']['display']['title'],
-            'creationdate': d['pnx']['display']['creationdate'] if d['pnx']['display'].get('creationdate') else '',
-            'creator': d['pnx']['display']['creator'] if d['pnx']['display'].get('creator') else '',
-            'publisher': d['pnx']['display']['publisher'],
-            'description': d['pnx']['display']['description'] if d['pnx']['display'].get('description') else ''
-        } for d in doc
+            'type': d['pnx']['display']['type'][0],
+            'title': d['pnx']['display']['title'][0],
+            'publisher': d['pnx']['display']['publisher'][0],
+            'description': d['pnx']['display']['description'][0] if d['pnx']['display'].get('description') else '',
+            'link': requests.post(resource_url.replace('[insert_id_here]', d['pnx']['control']['recordid'][0]), data=d).url,
+            'summary': ''
+        } for d in docs
     ]
-    return {'Resources': result[0]}
+    # TODO: Get Summary
+    # TODO: Get Link
+    return {'Resources': result}
 
 if __name__ == "__main__":
     app.run()
