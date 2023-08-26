cf71db3fc3aec6286c48616ff21129c869aea965
Wei Bin <auyeongweibin@gmail.com>
2023-08-26 15:56:24 +0800

    added resource links

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ from flask_cors import CORS
 import requests
 from config import SEARCH_URL, GET_RESOURCE
 
+def get_url(doc):
+    resource_url = GET_RESOURCE
+    url = ''
+    r =requests.post(resource_url.replace('[insert_id_here]', doc['pnx']['control']['recordid'][0]), json={'doc':doc})
+    if r.status_code == 200:
+        url = r.json().get("redirect_to")
+    return url
+
+
 def create_app(config_filename):
     application = app = Flask(__name__, static_url_path='')
     application.config.from_object(config_filename)
@@ -23,7 +32,6 @@ def search():
     key = request.args.get('key')
     search_key = key.replace(' ', '+')
     url = SEARCH_URL.replace('[insert_search_here]', search_key)
-    resource_url = GET_RESOURCE
     res = requests.get(url).json()
     docs = res['docs']
     if len(docs) == 0:
@@ -34,7 +42,7 @@ def search():
             'title': d['pnx']['display']['title'][0],
             'publisher': d['pnx']['display']['publisher'][0],
             'description': d['pnx']['display']['description'][0] if d['pnx']['display'].get('description') else '',
-            'link': requests.post(resource_url.replace('[insert_id_here]', d['pnx']['control']['recordid'][0]), data=d).url,
+            'link': get_url(d),
             'summary': ''
         } for d in docs
     ]
